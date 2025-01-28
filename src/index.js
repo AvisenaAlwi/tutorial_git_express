bf0c952d67af4d2ebe59ba3b61f73406de8b3838
Avisena Alwi <avisena.abdillah98@gmail.com>
2025-01-28 20:35:15 +0700

    [add] route about

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! Branch About');
+});
+
+app.get('/about', (req, res) => {
+    res.send('Hallo ini halaman About');
 });
 
 app.listen(port, () => {
